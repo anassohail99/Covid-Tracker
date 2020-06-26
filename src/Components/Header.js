e83f291473f3b84d83dc6977bba9add84854ae24
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-27 01:35:58 +0500

    Main Header Component

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="heading"> GLOABL CONDITION OF COVID-19</h1>
+    </div>
+  );
+};
+
+export default Header;

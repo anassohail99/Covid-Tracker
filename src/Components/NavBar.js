b240702ccec545258c4ef7c883010054c0b6e5d2
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-27 01:36:29 +0500

    NavBar Component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" style={{ background: "#000" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          />
+          <Typography variant="h6" className={classes.title}>
+            COVID-19 TRACKER
+          </Typography>
+          {/* <Link to="/about" component/> */}
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Stats</Button>
+          <Button color="inherit">Country</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}

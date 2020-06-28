b56ce870bf8cb615fb5cbcd1a9e47291032baed1
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-29 01:56:24 +0500

    Buttons replaced with links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,12 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import "../App.css";
 import About from "./About/About";
 import Home from "./Home/Home";
 import Country from "./Country/Country";
+import logo from "./assets/virus.png";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -32,7 +33,11 @@ export default function ButtonAppBar() {
   return (
     <div className={classes.root}>
       <Router>
-        <AppBar position="fixed" style={{ background: "#271F55" }}>
+        <AppBar
+          className="Nav"
+          position="fixed"
+          style={{ background: "#271F55" }}
+        >
           <Toolbar>
             <IconButton
               edge="start"
@@ -41,6 +46,7 @@ export default function ButtonAppBar() {
               aria-label="menu"
             />
             <Typography variant="h6" className={classes.title}>
+              <img className="logo" src={logo} alt="" srcset="" />
               <Link className="btn" to="/">
                 COVID-19 TRACKER
               </Link>{" "}

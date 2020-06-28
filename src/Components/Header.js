e4e5c2d8eb77a2399d854b8ecf7bd6f906f7fe5d
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-29 01:55:58 +0500

    Updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,10 @@ import "../App.css";
 const Header = () => {
   return (
     <div>
-      <h1 className="heading"> GLOBAL CONDITION OF COVID-19</h1>
+      <h1 className="heading">
+        {" "}
+        GLOBAL CONDITION OF <span className="covid"> COVID-19 </span>
+      </h1>
     </div>
   );
 };

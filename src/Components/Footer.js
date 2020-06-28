a73ac50b18b9662f8d32cf89e5ec5f0fd33b129b
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-29 01:55:41 +0500

    Footer Component Created

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../App.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="cont">Made by Anas Bin Sohail</div>
+      <div className="contact">
+        <a href="https://github.com/anassohail99">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://www.facebook.com/anas.b.sohail">
+          <i class="fab fa-facebook"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/anas-bin-sohail-3a6168168/">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

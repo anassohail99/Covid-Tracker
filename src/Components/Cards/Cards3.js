6a1595394cd8d04ba3e1cc936ebf69ad38b77767
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-28 20:44:14 +0500

    Third Card Component Added

---
+++ src/Components/Cards/Cards3.js
@@ -0,0 +1,193 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import styles from "./Cards2.module.css";
+import cx from "classnames";
+// import loader from "../assets/loader.gif";
+
+const Cards3 = ({
+  data: { activeCases, newCases, newDeaths, totalCases, totalDeath, recovered },
+}) => {
+  if (!activeCases) {
+    return <h2>Getting Data...</h2>;
+  }
+  console.log(activeCases);
+  return (
+    // <h1>{data.activeCases}</h1>
+    <React.Fragment>
+      <div className={styles.container}>
+        <Grid container spacing={3} justify="center">
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Confirmed)}
+          >
+            {" "}
+            {/*we use cx to use multiple styles*/}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>Total Active Cases</Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(activeCases.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number of Active Cases of COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Deaths)}
+          >
+            {" "}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>New Deaths</Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(newDeaths.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number of New Deaths Due To COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Recovered)}
+          >
+            {" "}
+            {/*we use cx to use multiple styles*/}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>
+                Total Recoveries From COVID-19{" "}
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(recovered.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number Recoveries From COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Confirmed)}
+          >
+            {" "}
+            {/*we use cx to use multiple styles*/}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>Total Cases</Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(totalCases.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number of Total Cases of COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Deaths)}
+          >
+            {" "}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>Total Deaths</Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(totalDeath.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number of Total Deaths Due To COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={3}
+            className={cx(styles.card2, styles.Recovered)}
+          >
+            {" "}
+            {/*we use cx to use multiple styles*/}
+            <CardContent className={styles.cardContent}>
+              <Typography gutterBottom>New Cases Of COVID-19 </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={parseInt(newCases.replace(/,/g, ""))}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              {/* <Typography variant="h5">
+                {new Date(lastUpdate).toDateString()}
+              </Typography> */}
+              <Typography variant="body2">
+                Number Of New Cases Of COVID-19 In Pakistan{" "}
+              </Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Cards3;

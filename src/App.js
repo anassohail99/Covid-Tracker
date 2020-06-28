ce7f399853cc149b4c021313f014d9f563a311b8
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-28 17:48:25 +0500

    Generalized components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import Header from "./Components/Header";
-import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Header from "./Components/Header";
+// import Home from "./Components/Home/Home";
+// import About from "./Components/About/About";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <div className="header">
-          <Header />
-        </div>
-        <Home />
       </div>
     );
   }

73fd55aa06d674def8618f33b36e1d0dbaad0722
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-30 01:55:40 +0500

    Api for Country Data added

--- src/api/index.js
+++ src/api/index.js
@@ -77,6 +77,34 @@ export const fetchData3 = async () => {
   }
 };
 
+export const fetchData4 = async (country) => {
+  try {
+    const data = await axios.get(
+      `https://covid-19.dataflowkit.com/v1/${country}`
+    );
+    const dataArray = await Object.values(data.data);
+    const dataLength = dataArray.length;
+    const activeCases = dataArray[0];
+    const newCases = dataArray[3];
+    const newDeaths = dataArray[4];
+    const totalCases = dataArray[dataLength - 3];
+    const totalDeath = dataArray[dataLength - 2];
+    const recovered = dataArray[dataLength - 1];
+    console.log(data);
+    return {
+      activeCases,
+      newCases,
+      newDeaths,
+      totalCases,
+      totalDeath,
+      recovered,
+    };
+    // return dataArray;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
@@ -84,7 +112,19 @@ export const fetchDailyData = async () => {
       confirmed: dailyData.confirmed.total,
       deaths: dailyData.deaths.total,
       recovered: dailyData.recovered.total,
+      date: dailyData.reportDate,
     }));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }

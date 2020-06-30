3c5b6e247477a8796a28d61b70a413a73bce290d
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-30 23:25:19 +0500

    Api updated for cards4 and country page

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -3,11 +3,7 @@ import { fetchData } from "../../api/index";
 import CountryPicker from "../CountryPicker/CountryPicker";
 import CountryHeader from "./CountryHeader";
 import LineChart from "../Charts/line";
-import ApexLineChart from "../Charts/ApexLineChart";
-import Cards2 from "../Cards/Cards2";
-import Cards from "../Cards/Cards";
-import { fetchData2 } from "../../api/index";
-import { fetchData4 } from "../../api/index";
+import Cards4 from "../Cards/Card4";
 import styles from "./Country.module.css";
 
 class Country extends Component {
@@ -20,15 +16,11 @@ class Country extends Component {
   async componentDidMount() {
     const FetchData = await fetchData();
     this.setState({ data: FetchData });
-    const fetch_data3 = await fetchData2();
-    this.setState({ data2: fetch_data3 });
   }
 
   handleCountryChange = async (country) => {
     const FetchData = await fetchData(country);
     this.setState({ data: FetchData, country: country });
-    const FetchData2 = await fetchData4(country);
-    this.setState({ data2: FetchData2, country: country });
   };
 
   render() {
@@ -41,7 +33,7 @@ class Country extends Component {
           <div className={styles.picker}>
             <CountryPicker handleCountryChange={this.handleCountryChange} />
           </div>
-          <Cards2 data={data2} />
+          <Cards4 data={data} />
           <div className={styles.linechart}>
             <LineChart data={data} country={country} />
           </div>
